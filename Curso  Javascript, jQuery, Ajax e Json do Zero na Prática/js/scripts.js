9fa85d2f61fc10942cecce1ab60db45ac4209c81
Wilderlan Duarte <wilderlanduarte@gmail.com>
2023-12-04 15:41:47 -0300

    Strings

--- Curso  Javascript, jQuery, Ajax e Json do Zero na Prática/js/scripts.js
+++ Curso  Javascript, jQuery, Ajax e Json do Zero na Prática/js/scripts.js
@@ -1,5 +1,25 @@
+/*
+- ula-3 variaveis
+
 var latitude = 40.87663;
 var longitude = -8.08373;
 
 console.log(latitude)
 console.log(longitude);
+
+- Aula-4 Strings
+*/
+var nome = "João";  // podem ser usadas aspas duplas
+var sobrenome = 'Gomes';  // ou aspas simples
+var cpf = '111.111.111-11';  
+var telefone = "998887655";
+var ddd = '21';
+var email = 'joao@gmail.com';
+var biografia = "João começou a sua carreira como desenvolvedor web em 2010 e já participou de importantes projetos, como...";
+
+console.log('Nome completo: '+ nome + ' '+ sobrenome);
+console.log(ddd + '-'+telefone);
+console.log('('+ ddd + ')' + '-'+ telefone);
+console.log('Primeira letra do nome: ' + nome[0]);
+console.log('Ultima letra do nome: ' + nome[3]);
+console.log('Número de caracteres do telefone: '+ telefone.length);
